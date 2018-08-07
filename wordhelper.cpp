70b1d318487051abc66303dad08ff74f8f72e820
Frankie Betancourt <frankie.betanco@gmail.com>
2018-08-07 12:06:11 -0500

    Output strings sorted by length now

--- wordhelper.cpp
+++ wordhelper.cpp
@@ -24,19 +24,38 @@ class WordHelper{
 		void checkDict(); 
 };
 
+/* Sort completed strings by length */
+struct stringLen {
+  bool operator() (const string lhs_str, const string rhs_str) {
+    const size_t lhs_len = lhs_str.length(); 
+    const size_t rhs_len = rhs_str.length(); 
+   
+    /* If the length is the same, default to normal string sorting */
+    if ( lhs_len == rhs_len ) { 
+      return (rhs_str < lhs_str); 
+    }
+
+    return (rhs_len < lhs_len);
+  }
+};
+
 int main(int argc, char * argv[])
 {
 	WordHelper w;
 	
 	for (int i = 1; i < argc; i++) w.letters.push_back((char) *argv[i]); 
 
-	w.powerSetEnum();//enumerate all of the possible sets of words
-	w.permuteWords();//permute all of the possible sets
-	w.checkDict();   //check all of the permutation of sets against a dictionary
+  /* enumerate all of the possible sets of words */
+	w.powerSetEnum();
+  /* permute all of the possible sets */
+	w.permuteWords();
+  /* check all of the permutation of sets against a dictionary */
+	w.checkDict();
 	
 	return 0; 
 }
 
+/* Do a power set enumeration of all of the input letters */
 void WordHelper::powerSetEnum()
 {
 	string possibleWord; 
@@ -50,6 +69,7 @@ void WordHelper::powerSetEnum()
 	}
 }
 
+/* Find all of the permutations for all the strings in the power set */
 void WordHelper::permuteWords()
 {
 	string stringToPermute; 
@@ -63,16 +83,26 @@ void WordHelper::permuteWords()
 	}
 }
 
+/* Check all of the permutations against a dictionary */
 void WordHelper::checkDict()
 {
 	ifstream words; 
 	string word; 
 	set <string> dictionary; 
+  set <string, stringLen> valid_words;
 	
 	words.open("words.txt"); 
 
 	while(words >> word) dictionary.insert(word); 
 
+  /* Do comparison of all words and put in a set */
 	for(auto it = possibleWords.begin(); it != possibleWords.end(); it++)
-		if(dictionary.find(*it) != dictionary.end()) cout << *it << endl;
+		if (dictionary.find(*it) != dictionary.end()) {
+      valid_words.insert(*it);
+    }
+
+  /* Print out the list of valid words */
+  for ( auto it = valid_words.begin(); it != valid_words.end(); it++ ) {
+    cout << *it << endl;
+  }
 }
